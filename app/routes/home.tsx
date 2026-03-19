21fe7735e7b4819d7f62b2448391cf1086723716
Pranay Kumar Jha <pranayjha952@gmail.com>
2026-03-19 19:34:43 +0530

    Error in plans

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,14 @@
 import { ArrowRight, ArrowUpRight, Clock } from "lucide-react";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 import type { Route } from "./+types/home";
 import { Button } from "../components/ui/Button";
 import { Layers } from "lucide-react";
 import Upload from "../components/Upload";
 import { useNavigate } from "react-router";
+import { timeStamp } from "console";
+import { createProject } from "~/lib/puter.action";
+
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -14,9 +18,28 @@ export function meta({ }: Route.MetaArgs) {
 
 export default function Home() {
   const navigate = useNavigate();
+  const [projects, setProjects] = useState<DesignItem[]>([]);
   const handleUploadComplete = async (base64Image: string) => {
     const newId = Date.now().toString();
-    navigate(`/visualizer/${newId}`)
+    const name = `Residence ${newId}`;
+    const newItem = { id: newId, name, sourceImage: base64Image, renderedImage: undefined, timestamp: Date.now() }
+
+    const saved = await createProject({ item: newItem, visibility: 'private' });
+
+    if (!saved) {
+      console.error("Failed to create a project");
+      return false;
+    }
+    setProjects((prev) => [newItem, ...prev]);
+
+    navigate(`/visualizer/${newId}`, {
+      state: {
+        initialImage: saved.sourceImage ,
+        initialRendered:saved.renderedImage || null,
+        name
+
+      }
+    });
     return true;
   };
   return <div className="home">
@@ -63,18 +86,19 @@ export default function Home() {
           </div>
         </div>
         <div className="projects-grid ">
+          {projects.map(({id,name,renderedImage,sourceImage,timestamp})=>(
           <div className="project-card group">
             <div className="preview">
-              <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project" />
+              <img src={renderedImage || sourceImage}alt="Project" />
               <div className="badge">
                 <span>Community</span>
               </div>
             </div>
             <div className="card-body">
-              <div><h3>Project JSR</h3>
+              <div><h3>{name}</h3>
                 <div className="meta">
                   <Clock size={12} />
-                  <span>{new Date('2027-01-01').toLocaleDateString()}</span>
+                  <span>{new Date(timestamp).toLocaleDateString()}</span>
                   <span>By Pranay</span>
                 </div>
               </div>
@@ -85,7 +109,7 @@ export default function Home() {
             </div>
 
           </div>
-
+          ))}
         </div>
       </div>
     </section>
